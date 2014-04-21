9c5368ef49d34d7d4be6fc3d1e4d0cbd664a28db
Ethan Reesor <ethan.reesor@firelizzard.com>
2014-04-21 17:13:59 -0400

    Adding usage

--- MathSpice/MathSpice.m
+++ MathSpice/MathSpice.m
@@ -6,16 +6,21 @@ SpiceHelp::usage="SpiceHelp[] prints usage information for various MathSpice fun
 SpiceConnect::usage="SpiceConnect[] links the MathSpice executable";
 SpiceDisconnect::usage="SpiceDisconnect[] unlinks the MathSpice executable";
 
-SpiceTransientAnalysis::usage="";
-SpiceACAnalysis::usage="";
+SpiceTransientAnalysis::usage="SpiceTransientAnalysis[file, step, stop] links MathSpice, sources the file, performs a transient analysis, saves the data, unlinks MathSpice, and returns the data";
+SpiceACAnalysis::usage="SpiceTransientAnalysis[file, start, stop] links MathSpice, sources the file, performs an AC analysis, saves the data, unlinks MathSpice, and returns the data";
 
-SpiceAnalysistBranches::usage="";
+SpiceAnalysisBranches::usage="SpiceAnalysisBranches[data] lists the branches available in the data set. SpiceAnalysisBranches[] calls SpiceAnalysisBranches[data] on the last set of calculated data.";
 SpiceAnalysisBranchIndexForName::usage="";
-SpiceAnalysisBranchData::usage="";
+SpiceAnalysisBranchData::usage="SpiceAnalysisBranchData[data, name] returns the data set for the named branch from the data set. SpiceAnalysisBranchData[name] calls SpiceAnalysisBranchData[data, name] on the last set of calculated data.";
 
 Begin["`Private`"];
 
-SpiceHelp[]:=(Information[#,LongForm->False]&/@Join[{SpiceHelp},#[[1,0]]&/@LinkPatterns[link]];Null);
+SpiceHelp[]:=Module[{link},
+Information[#,LongForm->False]&/@{SpiceHelp,SpiceTransientAnalysis,SpiceACAnalysis,SpiceAnalysisBranches,SpiceAnalysisBranchData};
+link=Install["MathSpice`"];
+Information[#,LongForm->False]&/@LinkPatterns[link][[;;,1,0]];
+Uninstall[link];
+];
 
 (*link=Null;
 SpiceConnect[]:=(link=Install["MathSpice"];Null);
@@ -37,13 +42,12 @@ Uninstall[link];
 lastData
 ];
 
-SpiceAnalysistBranches[]:=SpiceAnalysistBranches[lastData];
-SpiceAnalysistBranches[data_]:=Table[Rule[i,#[[i]]],{i,Length@#}]&@data[[2;;,1]];
+SpiceAnalysisBranches[]:=SpiceAnalysisBranches[lastData];
+SpiceAnalysisBranches[data_]:=Table[Rule[i,#[[i]]],{i,Length@#}]&@data[[2;;,1]];
 SpiceAnalysisBranchIndexForName[name_]:=SpiceAnalysisBranchIndexForName[lastData,name];
 SpiceAnalysisBranchIndexForName[data_,name_]:=Position[data[[2;;,1]],name][[1,1]];
 SpiceAnalysisBranchData[name_]:=SpiceAnalysisBranchData[lastData,name];
 SpiceAnalysisBranchData[data_,name_]:=ReplacePart[data[[SpiceAnalysisBranchIndexForName[data,name]+1,2;;]],0->List];
-
 
 End[];
 
